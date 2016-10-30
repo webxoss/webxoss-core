514968d51e8a80e467888570952041374877a372
WEBXOSS <webxoss@gmail.com>
2016-10-31 01:35:03 +0800

    Deploy

--- RoomManager.js
+++ RoomManager.js
@@ -1,7 +1,7 @@
 'use strict';
 
 function RoomManager (cfg) {
-	this.VERSION              = 63;
+	this.VERSION              = 64;
 
 	this.MAX_ROOMS            = cfg.MAX_ROOMS;
 	this.MAX_CLIENTS          = cfg.MAX_CLIENTS;
