6789a67a5baac485bec536c81f3159e635eb2169
WEBXOSS <webxoss@gmail.com>
2017-08-26 13:09:37 +0800

    🐛 fix spell selecting target

--- Player.js
+++ Player.js
@@ -574,14 +574,14 @@ Player.prototype.handleSpellAsyn = function (card,ignoreCost,costObj,arg) {
 			// 简单的取对象,即从目标卡片中选一张. (也可以不选,空发)
 			if (effect.targetCovered) {
 				// 从废弃区等[卡片可能被覆盖的区域]取对象
-				return this.selectOptionalAsyn('TARGET',effect.getTargets.call(card)).callback(this,function (card) {
+				return this.selectAsyn('TARGET',effect.getTargets.call(card)).callback(this,function (card) {
 					if (!card) return card;
 					return this.opponent.showCardsAsyn([card]).callback(this,function () {
 						return card;
 					});
 				});
 			}
-			return this.selectTargetOptionalAsyn(effect.getTargets.call(card));
+			return this.selectTargetAsyn(effect.getTargets.call(card));
 		}
 		if (effect.getTargetAdvancedAsyn) {
 			// 复杂(高级)的取对象.
